654c3fdbabb30e70e3e5007d045f4f861a8bfff3
Sean Chain <seanchain2.0@gmail.com>
2015-09-01 09:47:02 +0800

    For syncing

--- public/js/jq.js
+++ public/js/jq.js
@@ -1,27 +1,23 @@
 $(function() {
 
-  window.addEventListener('keydown', function (event) {
-    if(event.keyCode == 13) {
-      $("#submit").click();
-    }
-  });
+	window.addEventListener('keydown', function(event) {
+		if (event.keyCode == 13) $("#submit").click();
+	});
 
 
-  $("#submit").click(function () {
-    var userid = $("#userid").val();
-    var email = $("#email").val();
-    var passwd = $("#passwd").val();
-    console.log(userid + "---" + email + "---" + passwd);
-    if (userid == '123') {
-      console.log("got it");
-      $.post('/checkValidation', {userid:userid}, function(res) {
-        if (res == "success") {
-          window.location.href = "/";
-        }
-        else {
-          alert("wrong!");
-        }
-      });
-    }
-  });
+	$("#submit").click(function() {
+		var userid = $("#userid").val();
+		var email = $("#email").val();
+		var passwd = $("#passwd").val();
+		console.log(userid + "---" + email + "---" + passwd);
+		if (userid == '123') {
+			console.log("got it");
+			$.post('/checkValidation', {
+				userid: userid
+			}, function(res) {
+				if (res == "success") window.location.href = "/";
+				else alert("wrong!");
+			});
+		}
+	});
 });
